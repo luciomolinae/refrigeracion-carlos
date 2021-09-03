aa3c12159d979d735f5408e087b4b675b3671b00
Lucio Molina <luzxiob3@gmail.com>
2021-09-03 11:04:19 -0300

    arreglo del formulario

--- enviarFormulario.php
+++ enviarFormulario.php
@@ -1,52 +1,24 @@
 <?php
-function form_mail($sPara, $sAsunto, $sTexto, $sDe) {
-    $bHayFicheros = 0;
-    $sCabeceraTexto = "";
-    $sAdjuntos = "";
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-    if ($sDe)$sCabeceras = "From:".$sDe."\n";
-    else $sCabeceras = "";
-    $sCabeceras .= "MIME-version: 1.0\n";
-    $sCabeceras .= "Content-Type: text/plain; charset=UTF-8"; 
-    foreach ($_POST as $sNombre => $sValor)
-        $sTexto = $sTexto."\n".$sNombre." = ".$sValor;
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-        foreach ($_FILES as $vAdjunto) {
-            if ($bHayFicheros == 0) {
-                $bHayFicheros = 1;
-				$cabeceras = "MIME-Version: 1.0\r\n";
-				$sCabeceras .= "Content-Type: text/plain; charset=UTF-8\r\n";
-				$cabeceras .= "Content-Transfer-Encoding: 8bit\r\n";
-                $sCabeceras .= "Content-type: multipart/mixed;";
-                $sCabeceras .= "boundary=\"--_Separador-de-mensajes_--\"\n";
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
 
-                $sCabeceraTexto = "----_Separador-de-mensajes_--\n";
-                $sCabeceraTexto .= "Content-Type: text/plain; charset=UTF-8\r\n";
-                $sCabeceraTexto .= "Content-transfer-encoding: 7BIT\n";
+$para = 'cardasilva99@gmail.com';
+$asunto = 'Mensaje de... ';
 
-                $sTexto = $sCabeceraTexto.$sTexto;
-            }
+mail($para, $asunto, utf8_decode($message), $header);
 
-            if ($vAdjunto["size"] > 0) {
-                $sAdjuntos .= "\n\n----_Separador-de-mensajes_--\n";
-                $sAdjuntos .= "Content-type: ".$vAdjunto["type"].";name=\"".$vAdjunto["name"]."\"\n";;
-                $sAdjuntos .= "Content-Transfer-Encoding: BASE64\n";
-                $sAdjuntos .= "Content-disposition: attachment;filename=\"".$vAdjunto["name"]."\"\n\n";
-
-                $oFichero = fopen($vAdjunto["tmp_name"], 'r');
-                $sContenido = fread($oFichero, filesize($vAdjunto["tmp_name"]));
-                $sAdjuntos .= chunk_split(base64_encode($sContenido));
-                fclose($oFichero);
-            }
-        }
-
-    if ($bHayFicheros)
-        $sTexto .= $sAdjuntos."\n\n----_Separador-de-mensajes_----\n";
-    return(mail($sPara, mb_encode_mimeheader($sAsunto), $sTexto, $sCabeceras));
-}
-
-//Revisa los nombres de los campos de email y asunto
-if (form_mail("cardasilva99@gmail.com", $_POST[asunto],
-    "Asunto del Formulario:\n\n", $_POST[email]))
-    header("Location:index.html");
+header("Location:index.html");
 ?>
